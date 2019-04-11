161240f56f618c9cf64c1724478a041d6569cce4
Александр Гусев <alex1232131@mail.ru>
2019-04-11 19:45:09 +0700

    Add samples controller

--- src/server.js
+++ src/server.js
@@ -2,9 +2,11 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const ObjectId = require("mongodb").ObjectId;
 const assert = require('assert');
-
 const app = express();
 const jsonParser = express.json();
+const fs = require('fs');
+const archiver = require('archiver');
+const request = require('request');
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
@@ -23,6 +25,7 @@ mongoClient.connect(function(err, client) {
   dbClient = client;
   app.locals.employees = client.db("dcmdb").collection("employees");
   app.locals.instances = client.db("dcmdb").collection("instances");
+  app.locals.samples = client.db("dcmdb").collection("samples");
 
   app.listen(2019, function() {
     console.log('Server up!')
@@ -155,6 +158,88 @@ app.delete("/api/instances/:id", function(req, res) {
   collection.findOneAndDelete({_id: id}, function(err, result) {
     if (err) return console.log(err);
     res.send(result.value);
+  });
+});
+
+/*______________________________________________________________________________*/
+app.get("/api/samples/:id", function(req, res) {
+  let tmp = [];
+
+  var output = fs.createWriteStream(__dirname + '/example.zip');
+  var archive = archiver('zip', {
+    zlib: { level: 9 }
+  });
+
+  output.on('close', function() {
+    console.log(archive.pointer() + ' total bytes');
+    console.log('archiver has been finalized and the output file descriptor has closed.');
+    res.end();
+    // res.sendFile(__dirname + '/example.zip', function() {
+    //
+      tmp.forEach(function(item) {
+        fs.unlink(__dirname + '/' + item + '.dcm', function(err) {
+          if (err) throw err;
+        });
+      })
+
+    //   // fs.unlink(__dirname + '/example.zip', function(err) {
+    //   //   if (err) throw err;
+    //   //   console.log("Archive deleted");
+    //   // });
+    // });
+  });
+
+  output.on('end', function() {
+    console.log('Data has been drained');
+  });
+
+  archive.on('warning', function(err) {
+    if (err.code === 'ENOENT') {
+      // log warning
+    } else {
+      throw err;
+    }
+  });
+
+  archive.on('error', function(err) {
+    throw err;
+  });
+
+  archive.pipe(output);
+
+  const id = new ObjectId(req.params.id);
+  const collection = req.app.locals.samples;
+  collection.findOne({_id: id}, async function(err, result) {
+    if (err) return console.log(err);
+
+    result.instances.forEach(function(item) {
+      request('http://localhost:8042/instances/' + item + '/file').pipe(fs.createWriteStream(__dirname + '/' + item + '.dcm'));
+      tmp.push(item);
+      console.log("LOH");
+    });
+
+    setTimeout(function() {
+      tmp.forEach((item) => {
+        archive.file(__dirname + '/' + item + '.dcm', { name: item + '.dcm' });
+      })
+      archive.finalize();
+    }, 5000);
+  });
+})
+
+app.post("/api/samples", jsonParser, function(req, res) {
+  if (!req.body) return res.sendStatus(400);
+
+  const sample = {
+    username: req.body.username,
+    time: new Date().toLocaleString(),
+    instances: req.body.instances
+  };
+
+  const collection = req.app.locals.samples;
+  collection.insertOne(sample, function(err, result) {
+    if (err) return console.log(err);
+    res.send(result.ops);
   });
 });
 
