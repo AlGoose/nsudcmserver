1075a2a3e26302f5ce11ff69fb992c6ea237cf27
Александр Гусев <alex1232131@mail.ru>
2019-04-18 16:54:47 +0700

    Add zip archiving + fix samples controller

--- src/server.js
+++ src/server.js
@@ -173,20 +173,20 @@ app.get("/api/samples/:id", function(req, res) {
   output.on('close', function() {
     console.log(archive.pointer() + ' total bytes');
     console.log('archiver has been finalized and the output file descriptor has closed.');
-    res.end();
-    // res.sendFile(__dirname + '/example.zip', function() {
-    //
-      tmp.forEach(function(item) {
-        fs.unlink(__dirname + '/' + item + '.dcm', function(err) {
-          if (err) throw err;
-        });
-      })
+    // res.end();
+    res.sendFile(__dirname + '/example.zip', function() {
 
-    //   // fs.unlink(__dirname + '/example.zip', function(err) {
-    //   //   if (err) throw err;
-    //   //   console.log("Archive deleted");
-    //   // });
-    // });
+    tmp.forEach(function(item) {
+      fs.unlink(__dirname + '/' + item + '.dcm', function(err) {
+        if (err) throw err;
+      });
+    })
+
+      fs.unlink(__dirname + '/example.zip', function(err) {
+        if (err) throw err;
+        console.log("Archive deleted");
+      });
+    });
   });
 
   output.on('end', function() {
@@ -210,20 +210,21 @@ app.get("/api/samples/:id", function(req, res) {
   const id = new ObjectId(req.params.id);
   const collection = req.app.locals.samples;
   collection.findOne({_id: id}, async function(err, result) {
+    let counter = 0;
     if (err) return console.log(err);
 
     result.instances.forEach(function(item) {
-      request('http://localhost:8042/instances/' + item + '/file').pipe(fs.createWriteStream(__dirname + '/' + item + '.dcm'));
+      request('http://localhost:8042/instances/' + item + '/file').pipe(fs.createWriteStream(__dirname + '/' + item + '.dcm').on('finish', function() {
+        counter++;
+        if(counter == result.instances.length){
+          tmp.forEach((item) => {
+              archive.file(__dirname + '/' + item + '.dcm', { name: item + '.dcm' });
+          })
+          archive.finalize();
+        }
+      }));
       tmp.push(item);
-      console.log("LOH");
     });
-
-    setTimeout(function() {
-      tmp.forEach((item) => {
-        archive.file(__dirname + '/' + item + '.dcm', { name: item + '.dcm' });
-      })
-      archive.finalize();
-    }, 5000);
   });
 })
 
@@ -240,6 +241,16 @@ app.post("/api/samples", jsonParser, function(req, res) {
   collection.insertOne(sample, function(err, result) {
     if (err) return console.log(err);
     res.send(result.ops);
+  });
+});
+
+app.delete("/api/samples/:id", function(req, res) {
+  console.log(req.params.id);
+  const id = new ObjectId(req.params.id);
+  const collection = req.app.locals.samples;
+  collection.findOneAndDelete({_id: id}, function(err, result) {
+    if (err) return console.log(err);
+    res.send(result.value);
   });
 });
 
